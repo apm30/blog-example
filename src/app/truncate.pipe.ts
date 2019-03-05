a81f4c8c754cd2973a5b8fd1525999ff9d958d51
Domo <stolarczyk.dominik@gmail.com>
2019-03-05 07:58:04 +0100

    truncate fix and adding margin

--- src/app/truncate.pipe.ts
+++ src/app/truncate.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncatePipe implements PipeTransform {
   transform(value: any, args?: any): any {
-    const limit = args.length > 0 ? parseInt(args[0], 10) : 10;
-    const trail = args.length > 1 ? args[1] : '...';
+    const limit = args;
+    const trail = '...';
 
     return value.length > limit ? value.substring(0, limit) + trail : value;
   }
