ff0946cfd050d792277b9fc14bb9d9799c46ae04
Domo <stolarczyk.dominik@gmail.com>
2019-03-06 08:28:26 +0100

    Adding shell component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { TruncatePipe } from './truncate.pipe';
 import { PaginatorComponent } from './paginator/paginator.component';
 import { BlogDataService } from './blog-data.service';
 import { HighlightDirective } from './highlight.directive';
+import { CardComponent } from './card/card.component';
+import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { HighlightDirective } from './highlight.directive';
     BlogListComponent,
     TruncatePipe,
     PaginatorComponent,
-    HighlightDirective
+    HighlightDirective,
+    CardComponent,
+    ShellComponent
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [TruncatePipe, BlogDataService],
